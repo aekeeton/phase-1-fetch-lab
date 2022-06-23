cc47acfe4e403bae73594a5f310013d2ed8f71cb
aekeeton <ashley.keeton@outlook.com>
2022-06-23 17:52:18 -0500

    commit tree 98b3c4c10e6a22e2129a9ac69c18c22b63f3ab1f

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const bookPath = "https://anapioficeandfire.com/api/books"
+
+function getResponse(response) {
+  return response.json()
+}
+
+function processJSON(json) {
+  for(let i = 0; i < json.length; i ++) {
+    console.log(json[i])
+  }
+}
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch(bookPath)
+    .then((resp) => resp.json())
+    .then((books) => renderBooks(books))
 }
 
 function renderBooks(books) {
@@ -10,7 +24,7 @@ function renderBooks(books) {
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
-}
+} 
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
